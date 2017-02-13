ead2f27b45021a99ad55e90952d633f4f03d5047
Guruparan <cns_guru@yahoo.com>
2017-02-12 22:14:40 -0500

    working

--- js/app.js
+++ js/app.js
@@ -23,29 +23,21 @@ var results;
 var personID = 44;
 var castQueryURL;
 
-var actorAndMovies = [{		// main data object to hold actors, movies & pairings
-		actorFirstName: "",
-		actorLastName: "",
-		actorID:"",
-		actorMovies: [{
-			movieName:"",
-			movieID:"",
-			moviePair:"",
-			moviePairID:""
-		}]
-	}];
+var actorAndMovies = [{   // main data object to hold actors, movies & pairings
+    actorFirstName: "",
+    actorLastName: "",
+    actorID:"",
+    actorMovies: [{
+      movieName:"",
+      movieID:"",
+      moviePair:"",
+      moviePairID:""
+    }]
+  }];
 
-var temp = {			// temp variable to hold movie and cast information to be pushed
-	movieTitle:"",
-	movieID:"",
-	moviePair:"",
-	moviePairID:""
-};
-
-// database.ref().set(actorAndMovies);   // use to create first object in database
+database.ref().set(actorAndMovies);   // use to create first object in database
 
 // console.log("after first database set");
-// console.log(actorAndMovies[0].actorMovies);
 
 var movieID = 88;
 var movieTitle;
@@ -55,166 +47,129 @@ $("#main-actor-name").empty();  // clear the screen of any residue
 
 // Capture entered information about name of actor
 $("#find-cast-info").on("click", function() {
+  // main data object to hold actors, movies & pairings
+  // clear it to get it ready to hold info about this actor
+  actorAndMovies[0].actorFirstName = "";   
+  actorAndMovies[0].actorLastName = "";
+  actorAndMovies[0].actorID = "";
+  actorAndMovies[0].actorMovies = [];
 
-	$("#costars-movies").empty();  // clear the screen of any residue
-	$("#main-actor-name").empty();  // clear the screen of any residue
+  $("#costars-movies").empty();  // clear the screen of any residue
+  $("#main-actor-name").empty();  // clear the screen of any residue
 
-	// get first and last name of actor for whom cast information is to be found
-	firstName = $("#first-name-input").val().trim();
-	lastName = $("#last-name-input").val().trim();
+  // get first and last name of actor for whom cast information is to be found
+  firstName = $("#first-name-input").val().trim();
+  lastName = $("#last-name-input").val().trim();
 
-	// Clear the textbox when done
-	$("#first-name-input").val("");
-	$("#last-name-input").val("");
+  // Clear the textbox when done
+  $("#first-name-input").val("");
+  $("#last-name-input").val("");
 
-	// Build queryURL to get person ID of the actor being querried
-	queryURL = "https://api.themoviedb.org/3/search/person?api_key=47aaffc2100101b29b819d304b4a2a0d&language=en-US&query=" + firstName + " " + lastName + "&include_adult=false";
+  // Build queryURL to get person ID of the actor being querried
+  queryURL = "https://api.themoviedb.org/3/search/person?api_key=47aaffc2100101b29b819d304b4a2a0d&language=en-US&query=" + firstName + " " + lastName + "&include_adult=false";
+  // get person_id of actor from TMDB
+  $.ajax({  
+    url: queryURL,  
+    method: "GET",
+    async: false  // wait for ajax call to return
+  }) 
+  .done(function(response) {   // done getting ID  of actor entered
+    console.log("ID of " + firstName + " " + lastName + " retrieved");
+    // console.log(actorAndMovies[0].actorMovies);
+    // console.log(response.results);
+    if ( response.results.length > 0 ) {
+      // something valid came back from TMDB
+      results = response.results;
+      personID = results[0].id;
+      actorAndMovies[0].actorID = personID;
+      actorAndMovies[0].actorFirstName = firstName;
+      actorAndMovies[0].actorLastName = lastName;
 
-	// console.log("queryURL = " + queryURL);
+      var tempNameDiv = $("<div>");
+      tempNameDiv.addClass("actorNameColumn");
+      tempNameDiv.addClass("large-4 medium-4 small-4 columns");
+      tempNameDiv.html(firstName + " " + lastName);
+      // Put the name of actor on screen
+      $("#main-actor-name").append(tempNameDiv);
+      getNamesOfMovies(personID);
+      database.ref().push(actorAndMovies);
+      console.log(actorAndMovies);
+    }  // if something received for cast of member
+    else {
+      alert("Wrong Info");  // actor name is incorrect
+    }
+  });    
+});
 
-	// get person_id of actor from TMDB
-	$.ajax({  
-		url: queryURL,  
-		method: "GET",
-		async: false  // wait for ajax call to return
-	}) 
-	.done(function(response){   // done getting ID  of actor entered
-		console.log("ID of " + firstName + " " + lastName + " retrieved");
-		// console.log(actorAndMovies[0].actorMovies);
-		// console.log(response.results);
-		if (response.results.length >0 ) {
-			results = response.results;
-			// console.log("results");
-			// console.log(results);
-			personID = results[0].id;
-			// console.log("id of entered actor = " + personID);
+// ---------------------------------------------
 
-			// console.log(actorAndMovies);
+function getNamesOfMovies(actorTmdId) {
+  // Build queryURL to get movies acted by person being queried
+  var newQueryURL = "https://api.themoviedb.org/3/person/" + actorTmdId + "/movie_credits?api_key=47aaffc2100101b29b819d304b4a2a0d&language=en-US";
+  $.ajax({
+    url: newQueryURL,
+    method: "GET",
+    async: false  // make other code wait for ajax to return
+  })
+  .done(function(movieNames) {  // done getting movies acted by actor queried
+    var numberOfMoviesActed = movieNames.cast.length;
+    getCastOfMovies(movieNames, numberOfMoviesActed);
+  })
+}  // getNamesOfMovies function
 
-			actorAndMovies[0].actorID = personID;
-			actorAndMovies[0].actorFirstName = firstName;
-			actorAndMovies[0].actorLastName = lastName;
-			// database.ref().update(actorAndMovies);
+// ---------------------------------------------
 
-			var tempNameDiv = $("<div>");
-			tempNameDiv.addClass("actorNameColumn");
-			tempNameDiv.addClass("large-4 medium-4 small-4 columns");
-			tempNameDiv.html(firstName + " " + lastName);
-			$("#main-actor-name").append(tempNameDiv);
+function getCastOfMovies(namesOfMovies, numberOfMovies) {
+  for ( i = 0; i < numberOfMovies; i ++) {
+    movieTitle = namesOfMovies.cast[i].title;
+    movieID = namesOfMovies.cast[i].id;
+    var localMovieID = movieID;
+    getCostarMovieDetails(localMovieID);
+  }
+}
 
-			// Build queryURL to get movies acted by person being queried
-			newQueryURL = "https://api.themoviedb.org/3/person/" + personID + "/movie_credits?api_key=47aaffc2100101b29b819d304b4a2a0d&language=en-US";
-			// console.log("after getting person ID query = " + newQueryURL);
+// ---------------------------------------------
 
-			$.ajax({
-				url: newQueryURL,
-				method: "GET",
-				async: false 	// make other code wait for ajax to return
-			})
-			.done(function(movieNames) {  // done getting movies acted by actor entered
-				// console.log("movieNames");
-				// console.log(movieNames);
-				var numberOfMoviesActed = movieNames.cast.length;
-				// console.log("numberOfMoviesActed = " + numberOfMoviesActed);
-				// console.log(actorAndMovies[0].actorMovies);
-				// var j = 1;
-				for (var i = 0; i < numberOfMoviesActed; i ++) {
-				// for (var i = 0; i < 2; i ++) {
+function getCostarMovieDetails(lMovieID) {
+  var castQueryURL = "https://api.themoviedb.org/3/movie/" + lMovieID + "/credits?api_key=47aaffc2100101b29b819d304b4a2a0d";
+  $.ajax({
+    url:castQueryURL,
+    method: "GET",
+    async: false  // make other code wait for ajax to return
+  })
+  .done(function(castOfMovies) {
+    // temp variable to hold movie and cast information to be pushed
+    var temp = {
+      movieTitle:"",
+      movieID:"",
+      moviePair:"",
+      moviePairID:""
+    };
+    var numberOfCast = castOfMovies.cast.length;
+    if (numberOfCast > 1) {
+      temp.movieTitle = movieTitle;
+      temp.movieID = movieID;
+      temp.moviePair = castOfMovies.cast[1].name;
+      temp.moviePairID = castOfMovies.cast[1].id;
 
-					// console.log("JUST INSIDE NUMBER MOVIES ACTED");
+      // Put name of CoStar on screen
+      var tempNamePairDiv = $("<div>");
+      tempNamePairDiv.addClass("costarNameColumn");
+      tempNamePairDiv.addClass("large-7 medium-7 small-7 columns");
+      tempNamePairDiv.html(temp.moviePair);
+      $("#costars-movies").append(tempNamePairDiv);
 
-					// for (var x = 0; x < actorAndMovies[0].actorMovies.length; x ++) {
-					// 	console.log("x = "+ x + "  " + actorAndMovies[0].actorMovies[x].movieTitle);
-					// }
+      // Put name of Movie on screen
+      var tempNameMovieDiv = $("<div>");
+      tempNameMovieDiv.addClass("costarNameColumn");
+      tempNameMovieDiv.addClass("large-5 medium-5 small-5 columns");
+      tempNameMovieDiv.html(temp.movieTitle);
+      $("#costars-movies").append(tempNameMovieDiv);
 
-					movieTitle = movieNames.cast[i].title;
-					movieID = movieNames.cast[i].id;
-					// console.log("movies acted i = " + i);
-					// console.log(actorAndMovies[0].actorMovies);
-					// console.log("title of movie " + j + " = " + movieTitle);
-					// console.log(actorAndMovies[0].actorMovies);
-					// console.log("ID of movie " + j + " = " + movieID);
-					// j ++;
+      // store name of movie, name of costar, id of movie, id of costar
+      actorAndMovies[0].actorMovies.push(temp);    
+    }
+  })
+}
 
-					// build castQueryURL to get cast of the first movie
-					castQueryURL = "https://api.themoviedb.org/3/movie/" + movieID + "/credits?api_key=47aaffc2100101b29b819d304b4a2a0d";
-
-					$.ajax({
-						url: castQueryURL,
-						method: "GET",
-						async: false	// make other code wait for ajax to return
-					})
-					.done(function(castOfMovies) {
-						// console.log("castOfMovies");
-						var numberOfCast = castOfMovies.cast.length;
-						// console.log("AFTER CAST number of cast members = " + numberOfCast);
-						// console.log(actorAndMovies[0].actorMovies);
-						// var k = 1;
-						// for (var m = 0; m < numberOfCast; m ++) {
-						// 	console.log("cast " + k +" = " + castOfMovies.cast[m].name);
-						// 	console.log("cast id = " + castOfMovies.cast[m].id);
-						// 	k ++;
-						// }
-						if (numberOfCast > 1) {
-
-							temp.movieTitle = movieTitle;
-							temp.movieID = movieID;
-							temp.moviePair = castOfMovies.cast[1].name;
-							temp.moviePairID = castOfMovies.cast[1].id;
-
-
-							var tempNamePairDiv = $("<div>");
-							tempNamePairDiv.addClass("costarNameColumn");
-							tempNamePairDiv.addClass("large-7 medium-7 small-7 columns");
-							tempNamePairDiv.html(temp.moviePair);
-							$("#costars-movies").append(tempNamePairDiv);
-
-							var tempNameMovieDiv = $("<div>");
-							tempNameMovieDiv.addClass("costarNameColumn");
-							tempNameMovieDiv.addClass("large-5 medium-5 small-5 columns");
-							tempNameMovieDiv.html(temp.movieTitle);
-							$("#costars-movies").append(tempNameMovieDiv);
-
-
-
-							// console.log(temp);
-							// console.log("BEFORE length of array in object = " + actorAndMovies[0].actorMovies.length);
-							// for (var x = 0; x < actorAndMovies[0].actorMovies.length; x ++) {
-							// 	console.log("x = "+ x + "  " + actorAndMovies[0].actorMovies[x].movieTitle);
-							// }
-
-							// console.log(temp);
-							// console.log("BEFORE PUSH");
-							actorAndMovies[0].actorMovies.push(temp);
-							// console.log("var to store");
-							// console.log(actorAndMovies[0].actorMovies);
-							// console.log("AFTER PUSH");
-
-							// console.log("length of array in object = " + actorAndMovies[0].actorMovies.length);
-							// for (var x = 0; x < actorAndMovies[0].actorMovies.length; x ++) {
-							// 	console.log("x = "+ x + "  " + actorAndMovies[0].actorMovies[x].movieTitle);
-							// }
-
-						}
-					})
-				}  // go through all the movies acted
-				console.log("just before database push");
-				// console.log(actorAndMovies[0].actorMovies);
-				// database.ref().push(actorAndMovies);
-				// parse through relevant information received from IMDB
-
-				// Use Left 2/3 of the screen to place name of actor on the left side and names of co-starts on the right
-				//construct the DIVs with appropriate information and place them on the screen
-
-				// console.log("added class attribute to tempDiv");
-				// use right 1/3 of screen to place a link for users to buy movies starring one of the actors or one of the co-stars
-			});
-		// Don't refresh the page!
-		return false;
-		} else {
-			alert("Wrong Info");
-			// $('#myModal').foundation('reveal', 'open');
-		} // if something received for cast of members
-
-	});  // receiving ID of actor entered
-});  // click event done
+// ---------------------------------------------
